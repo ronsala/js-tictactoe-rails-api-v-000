296cbe1ef848dcdb307aea436dfb12474e04caf5
RonSala <ron.sala@gmail.com>
2020-03-30 18:00:21 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,13 @@
 // Code your JavaScript / jQuery solution here
+function player(turn = 0) {
+
+  if (turn % 2 == 0) {
+    return 'X';
+  } else {
+    return 'O';
+  }
+}
+
+function doTurn() {
+
+}
