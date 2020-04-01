6a1369c9e22bfe8814111f577ea4d61765eca282
RonSala <ron.sala@gmail.com>
2020-04-01 09:55:28 -0400

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,9 +1,19 @@
 // var square = '';
 // var squares = [];
 var turn = 0;
+var  winCombinations = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+];
 
 function attachListeners() {
-
+  
 }
 
 function checkWinner() {
@@ -20,10 +30,11 @@ var player = () => {
   } else {
     return 'O';
   }
-}
+};
 
-function setMessage() {
-
+function setMessage(string) {
+  var messageDiv = document.getElementById('message');
+  messageDiv.innerHTML = string;
 }
 
 function updateState(square) {
